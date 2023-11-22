de0adc6ff17321d8035967976169f1b66a8db75c
Noel Pena <127697218+noel-pena@users.noreply.github.com>
2023-11-22 18:33:28 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { writeFile } from 'node:fs';
+import { Buffer } from 'node:buffer';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
+
+let postText = ""
+
+const data = new Uint8Array(Buffer.from(`<%- include("partials_notes/header.ejs") %><h1><%= postEm %></h1><%- include("partials_notes/footer.ejs") %>`)) || {postEm: postText};
 
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 function logger(req, res, next){
-    postText = (req.body["log"])
+    postText = (req.body["log"]);
     next();
 }
 
@@ -25,17 +35,31 @@ app.get("/post", (req, res) => {
     res.render("post.ejs");
 });
 
-app.get("/logs", (req, res) => {
-    res.render("logs.ejs");
+let count = 0;
+
+app.use(logger)
+
+app.post("/posted", (req, res) => {
+    postText = (req.body["log"]);
+    res.render("posted.ejs", {postEm: postText});
+    writeFile('./views/notes/notes' + count + '.ejs', data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      }); 
+    count++;
 });
 
-app.post("/post", (req, res) => {
-    console.log(req.body)
-    logger();
-    res.render("index.ejs");
-    alert("your log was posted");
+let savedLogs = __dirname + "/views/notes/notes" + count + ".ejs";
+
+app.get("/notes", (req, res) => {
+    res.render("./notes/notes0.ejs", {postEm: postText});
 });
-  
+
+app.get("/logs", (req, res) => {
+    postText = (req.body["log"]);
+    res.render("logs.ejs", {saveEm: savedLogs, postEm: postText});
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
